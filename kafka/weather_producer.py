48b0d1648674371138e049a3f42e72d630315a07
NhatTan1232 <vovankhanh12345678@gmail.com>
2025-05-10 01:40:32 +0700

    added more provinces

--- kafka/weather_producer.py
+++ kafka/weather_producer.py
@@ -3,22 +3,29 @@ import requests
 import json
 import time
 import os
+from cities import city_to_id
+from dotenv import load_dotenv
 
+load_dotenv()
 API_KEY = os.getenv("WEATHER_API_KEY")  # from weatherapi.com or any API
-CITY = "Hanoi"
+cities = list(city_to_id.keys())
+print(API_KEY)
 producer = KafkaProducer(bootstrap_servers='localhost:9092',
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-def fetch_weather():
-    url = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={CITY}&aqi=no"
+def fetch_weather(city):
+    url = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}&aqi=no"
     response = requests.get(url)
     data = response.json()
-    data['location']['name'] = 'VNHN'
+    print(data)
+    data['location']['name'] = city_to_id[city]
     return data
 
 if __name__ == "__main__":
     while True:
-        weather_data = fetch_weather()
-        producer.send('weather-topic', weather_data)
-        print("Produced weather data:", weather_data)
-        time.sleep(10)  # every 30 mins (or shorter for testing)
+        for city in cities:
+            weather_data = fetch_weather(city)
+            if weather_data:
+                producer.send('weather-topic', weather_data)
+                print("Produced weather data:", weather_data)
+            time.sleep(1)
